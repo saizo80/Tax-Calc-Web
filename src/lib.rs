3b42975ec448ec4c05e1cf13511fd152b530c9a0
matt8094 <github@azerox.net>
2022-04-19 21:54:02 -0500

    update rust function name

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ April 19, 2022
 // Include the `wasm_bindgen` attribute into the current namespace
 use wasm_bindgen::prelude::wasm_bindgen;
 
+
 fn find_tax_target(target: f64, rate: f64) -> f64 {
     let mut input = target;
     while (((input+(input*rate))*100.0).round())/100.0 != target {
@@ -21,7 +22,7 @@ fn find_tax_target(target: f64, rate: f64) -> f64 {
     }
     return input;
 }
-
+#[allow(non_snake_case)]
 #[wasm_bindgen]
 pub fn rustInterface(input: &str) -> String {
     let rate: f64 = 0.07;
