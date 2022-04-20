02d3e09c58bb1c48f6f26b011651868d4236c47e
matt8094 <github@azerox.net>
2022-04-19 23:36:42 -0500

    add error display for negative numbers

--- src/lib.rs
+++ src/lib.rs
@@ -13,28 +13,31 @@ April 19, 2022
 // Include the `wasm_bindgen` attribute into the current namespace
 use wasm_bindgen::prelude::wasm_bindgen;
 
-
 fn find_tax_target(target: f64, rate: f64) -> f64 {
     let mut input = target;
     while (((input+(input*rate))*100.0).round())/100.0 != target {
         input -= 0.01;
         if input < 0.00 {
-            println!("Target could not be calculated");
-            std::process::exit(1);
+            return -0.03;
         }
     }
     return input;
 }
+
 #[allow(non_snake_case)]
 #[wasm_bindgen]
 pub fn rustInterface(input: &str, rate: &str) -> String {
     if input.parse::<f64>().is_ok() && rate.parse::<f64>().is_ok() { 
-        let number = input.parse().unwrap();
-        let tax_rate = (rate.parse::<f64>().unwrap())/100.0;
-            //println!("${:.2}", find_tax_target(number, rate));
-            return format!("${:.2}",find_tax_target(number, tax_rate));
+        let value: f64 = find_tax_target(input.parse().unwrap()
+            , rate.parse::<f64>().unwrap()/100.0);
+        if value != -0.03 {
+            return format!("{:.2}", value);
+        }
+        else{
+            return String::from("Error, could not find target amount.");
+        }
     }
     else {
-        return String::from("None");
+        return String::from("Error");
     }
 }
